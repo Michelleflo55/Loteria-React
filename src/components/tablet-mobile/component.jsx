5980920c2b8903be47230ed2608ad61a2a1fb724
Michelle Flores <lmfg2456@gmail.com>
2022-09-02 12:39:00 -0700

    added tablet mobile styling

---
+++ src/components/tablet-mobile/component.jsx
@@ -0,0 +1,10 @@
+import './tablet-mobile.styles.css'
+
+export const TabletMobile = () => {
+    return (
+        <div className='tablet-mobile'>
+            <p> Whoops! I am in tablet-mobile mode.</p>
+            <p> But if you see anything below me, I am now in Desktop mode</p>
+        </div>
+    )
+}
