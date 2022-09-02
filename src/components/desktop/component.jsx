47dd2a15db3af01579d8fa4f1642f68b6d97ae84
Michelle Flores <lmfg2456@gmail.com>
2022-09-02 12:40:27 -0700

    added classNames to component divs

--- src/components/desktop/component.jsx
+++ src/components/desktop/component.jsx
@@ -2,7 +2,7 @@ import './desktop.styles.css'
 
 export const Desktop = () => {
     return (
-        <div>
+        <div className='desktop'>
             <p> Whoops! I am in desktop mode.</p>
             <p> But if you see anything below me, I am now in Laptop mode </p>
         </div>
--- src/components/laptop/component.jsx
+++ src/components/laptop/component.jsx
@@ -2,7 +2,7 @@ import './laptop.styles.css'
 
 export const Laptop = () => {
     return (
-        <div>
+        <div className='laptop'>
             <p>Whoops! I am in laptop mode.</p>
             <p>But if you see anything below me, I am now in Big Screen mode</p>
         </div>
