35109fa51dc9a05f2839ac58c89b90897eaca52c
Michelle Flores <lmfg2456@gmail.com>
2022-09-02 12:27:58 -0700

    added sizing to desktop

---
+++ src/components/desktop/component.jsx
@@ -0,0 +1,10 @@
+import './desktop.styles.css'
+
+export const Desktop = () => {
+    return (
+        <div>
+            <p> Whoops! I am in desktop mode.</p>
+            <p> But if you see anything below me, I am now in Laptop mode </p>
+        </div>
+    )
+}
