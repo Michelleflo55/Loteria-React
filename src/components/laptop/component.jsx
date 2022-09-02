5778be3a972e418782f65124e754aff820a7fa17
Michelle Flores <lmfg2456@gmail.com>
2022-09-02 12:31:41 -0700

    added sizing to laptop

---
+++ src/components/laptop/component.jsx
@@ -0,0 +1,10 @@
+import './laptop.styles.css'
+
+export const Laptop = () => {
+    return (
+        <div>
+            <p>Whoops! I am in laptop mode.</p>
+            <p>But if you see anything below me, I am now in Big Screen mode</p>
+        </div>
+    )
+}
