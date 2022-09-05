ec584e0a985f006d91904ebe64d0e5728e6bd839
Michelle Flores <lmfg2456@gmail.com>
2022-09-05 12:29:04 -0700

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,42 +1,17 @@
-import { useMediaQuery } from 'react-responsive';
-import './App.css';
-import BigScreen from './components/BigScreen'
-import Desktop from './components/Desktop'
-import Laptop from './components/Laptop'
-import Mobile from './components/Mobile'
-import TabletMobile from './components/TabletMobile'
+import '../src/App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 
 function App() {
-  const isMobileDevice = useMediaQuery({
-    query: "(min-device-width: 480px)",
-  });
-
-  const isTabletDevice = useMediaQuery({
-    query: "(min-device-width: 768px)",
-  });
-
-  const isLaptop = useMediaQuery({
-    query: "(min-device-width: 1024px)",
-  });
-
-  const isDesktop = useMediaQuery({
-    query: "(min-device-width: 1200px)",
-  });
-
-  const isBigScreen = useMediaQuery({
-    query: "(min-device-width: 1201px )",
-  });
+  
   return (
+    
     <div>
-      <h1> React Responsive - a guide</h1>
-      {isMobileDevice && <Mobile />}
-      {isTabletDevice && <>
-      <TabletMobile />
-      {isDesktop && <Desktop />}
-      {isLaptop && <Laptop />}
-     {isBigScreen && <BigScreen />}
-</>}
+     
+      <p>Hello</p>
+    {/* <Route path='/' element={<Home />} /> */}
+
 
     </div>
   )
